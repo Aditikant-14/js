60eee372f952f1b0d20e30a81ab24031bc99122d
Aditi Kant <kantaditi587@gmail.com>
2024-10-25 09:59:10 +0530

    jhgjhkkj

---
+++ index.js
@@ -0,0 +1,11 @@
+let salaries - {
+    ankit: 1234,
+    preeti: 2345,
+    abhi: 1235
+}
+for (const key in salaries){
+    salaries[key] = ${salaries[key]}
+
+    console.log($key): ${salaries[key]}
+}
+console.log(salaries)
